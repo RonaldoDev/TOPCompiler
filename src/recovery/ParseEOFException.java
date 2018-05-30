32fde6f252c4b2d18257e86865bfea5a0683f981
ronaldodev <ronaldospranger@gmail.com>
2018-05-30 19:10:07 -0300

    version whithout loop

---
+++ src/recovery/ParseEOFException.java
@@ -0,0 +1,12 @@
+package recovery;
+
+public class ParseEOFException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParseEOFException(String x) {
+        super(x);
+    }
+}
