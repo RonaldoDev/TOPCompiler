61bdf05db3c7b9ff2e2554165f68250d82bb2fa6
ronaldodev <ronaldospranger@gmail.com>
2018-06-06 23:53:16 -0300

    change inheritance wizard pen

--- src/syntaticTree/ExpreNode.java
+++ src/syntaticTree/ExpreNode.java
@@ -2,7 +2,7 @@ package syntaticTree;
 
 import parser.Token;
 
-abstract public class ExpreNode extends GeneralNode {
+abstract public class ExpreNode extends StatementNode {
 	public ExpreNode(Token t) 
 	{
 		super(t);
